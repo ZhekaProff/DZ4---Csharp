3f94d2a3b722c7b4f55aab58b48be4a379e997c5
ZhekaProff <Zhekakad@ya.ru>
2022-05-20 14:16:32 +0900

    решение второй задачи

---
+++ Task02/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. 
+// Найти сумму положительных/отрицательных элементов массива
+Console.Clear();
+int [] array = new int [12];
+int length = array.Length; 
+int res = 0;
+RandomArray(array);
+PrintArray(array);
+
+for (int i = 0; i < length; i++) 
+res = res + array [i];
+
+Console.WriteLine();
+Console.WriteLine($"Result = {res}");
+
+
+
+
+// ====функции====
+void PrintArray(int[] array) // функция печати массива
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+void RandomArray(int[] array) // заполняем массив рандомными числами 
+{
+    int length = array.Length; // определяем длинну массива
+    for(int i = 0; i < length; i++ )
+    {
+        array[i] = new Random().Next(0, 9);     
+    }
+}
