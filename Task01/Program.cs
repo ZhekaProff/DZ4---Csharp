bc6fd572177a9700cf8952a33867f4d2b28fb1f3
ZhekaProff <Zhekakad@ya.ru>
2022-05-20 13:43:00 +0900

    решение первой задачи

---
+++ Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+Console.Clear();
+void PrintArray(int[] array) // функция печати массива
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+void RandomArray(int[] array) // заполняем массив рандомными числами 
+{
+    int length = array.Length; // определяем длинну массива
+    for(int i = 0; i < length; i++ )
+    {
+        array[i] = new Random().Next(0, 2);     
+    }
+}
+//========================Решение=============================
+
+int [] array = new int [8];
+RandomArray(array);
+PrintArray(array);
