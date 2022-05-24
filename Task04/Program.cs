d097174f82b6360dd7f34305a881d47acc28ff7c
ZhekaProff <Zhekakad@ya.ru>
2022-05-24 12:03:17 +0900

    решение 4 задачи

---
+++ Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+void RandomArray(int[] array) // заполняем массив рандомными числами 
+{
+    int length = array.Length; // определяем длинну массива
+    for(int i = 0; i < length; i++ )
+    {
+        array[i] = new Random().Next(0, 100);     
+    }
+}
+
+int [] array = new int [123];
+int a = 0;
+RandomArray(array);
+
+for ( int i = 0; i < array.Length; i++)
+    if (array[i] > 10 && array[i]< 99) a = a + 1;
+Console.Write($"Колличество элеметов: {a} ");    
+
